7916b9bd47e1325ccd5733794c3322f8f73a0467
Rayandson <rayandson_silva@hotmail.com>
2025-04-09 01:31:32 -0300

    feat: create notifications for ticket comments

--- src/modules/ticket-comment/ticket-comment.module.ts
+++ src/modules/ticket-comment/ticket-comment.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationModule } from '../notification/notification.module';
+import { UserModule } from '../user/user.module';
 import { TicketComment } from './entities/ticket-comment.entity';
 import { TicketCommentController } from './ticket-comment.controller';
 import { TicketCommentRepository } from './ticket-comment.repository';
 import { TicketCommentService } from './ticket-comment.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([TicketComment])],
+    imports: [TypeOrmModule.forFeature([TicketComment]), NotificationModule, UserModule],
     exports: [TicketCommentService],
     controllers: [TicketCommentController],
     providers: [TicketCommentService, TicketCommentRepository],
--- src/modules/ticket-comment/ticket-comment.service.ts
+++ src/modules/ticket-comment/ticket-comment.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { NotificationType } from '../notification/entities/notification.entity';
+import { NotificationRepository } from '../notification/notification.repository';
+import { UserRepository } from '../user/user.repository';
 import { CreateTicketCommentDto } from './dtos/create-ticket-comment.dto';
 import { UpdateTicketCommentDto } from './dtos/update-ticket-comment.dto';
 import { TicketComment } from './entities/ticket-comment.entity';
@@ -6,7 +9,11 @@ import { TicketCommentRepository } from './ticket-comment.repository';
 
 @Injectable()
 export class TicketCommentService {
-    constructor(private ticketCommentRepository: TicketCommentRepository) {}
+    constructor(
+        private ticketCommentRepository: TicketCommentRepository,
+        private userRepository: UserRepository,
+        private notificationRepository: NotificationRepository,
+    ) {}
 
     async findAll(): Promise<TicketComment[]> {
         return await this.ticketCommentRepository.find({ relations: ['user'] });
@@ -20,11 +27,58 @@ export class TicketCommentService {
     }
 
     async findBy(where: Partial<TicketComment>): Promise<TicketComment[]> {
-        return await this.ticketCommentRepository.find({ where, relations: ['user'] });
+        return await this.ticketCommentRepository.find({
+            where,
+            relations: ['user'],
+            order: { createdAt: 'DESC' },
+        });
     }
 
     async create(ticketComment: CreateTicketCommentDto) {
-        await this.ticketCommentRepository.save(ticketComment);
+        const [user, savedComment] = await Promise.all([
+            this.userRepository.findOne({
+                where: { id: ticketComment.userId },
+            }),
+            this.ticketCommentRepository.save(ticketComment),
+        ]);
+
+        const commentWithTicket = await this.ticketCommentRepository.findOne({
+            where: { id: savedComment.id },
+            relations: {
+                ticket: true,
+            },
+        });
+
+        const notifications = [];
+
+        if (ticketComment.userId !== commentWithTicket.ticket.requesterId) {
+            notifications.push(
+                this.notificationRepository.save({
+                    type: NotificationType.Comment,
+                    message: `Novo comentário de ${user.firstName} ${user.lastName}`,
+                    targetUserId: commentWithTicket.ticket.requesterId,
+                    resourceId: commentWithTicket.ticketId,
+                }),
+            );
+        }
+
+        if (
+            commentWithTicket.ticket.targetUserId &&
+            ticketComment.userId !== commentWithTicket.ticket.targetUserId
+        ) {
+            notifications.push(
+                this.notificationRepository.save({
+                    type: NotificationType.Comment,
+                    message: `Novo comentário de ${user.firstName} ${user.lastName}`,
+                    targetUserId: commentWithTicket.ticket.targetUserId,
+                    resourceId: commentWithTicket.ticketId,
+                }),
+            );
+        }
+
+        await Promise.all(notifications);
+
+        return commentWithTicket;
     }
 
     async update(id: number, ticketComment: UpdateTicketCommentDto) {
