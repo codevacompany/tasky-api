5ad5e1b5fe151bcc0e0f0fc6d76f94730c196f9b
Rayandson <rayandson_silva@hotmail.com>
2025-06-13 17:14:13 -0300

    feat: create change password endpoint

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,13 @@
-import { Body, Controller, Get, Patch, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { VerificationCodeService } from '../verification-code/verification-code.service';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dtos/login.dto';
 import { RefreshTokenDto } from './dtos/refresh-token.dto';
 import { ResetPasswordRequestDto } from './dtos/reset-password-request.dto';
-import { ResetPasswordDto } from './dtos/reset-password.dto';
 import { VerificationCodeValidationDto } from './dtos/verification-code-validation.dto';
-import { AccessProfile } from '../../shared/common/access-profile';
+import { AccessProfile, GetAccessProfile } from '../../shared/common/access-profile';
+import { ChangePasswordDto } from './dtos/change-password.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -28,6 +28,7 @@ export class AuthController {
         return user;
     }
 
+    //TODO: This endpoint is to be used when a user forgets their password
     // @Patch('reset-password')
     // @UseGuards(AuthGuard('jwt'))
     // async resetPassword(@Request() req, @Body() { password }: ResetPasswordDto) {
@@ -35,6 +36,8 @@ export class AuthController {
 
     //     return await this.authService.resetPassword(bearerToken, password);
     // }
+
+    //TODO: Implement an endpoint to change the password of a user
 
     @Post('reset-password/request')
     async requestPasswordReset(@Body() { email }: ResetPasswordRequestDto) {
@@ -45,12 +48,21 @@ export class AuthController {
 
     @Post('reset-password/validate')
     async validateVerificationCode(@Body() body: VerificationCodeValidationDto) {
-        const accessProfile = new AccessProfile()
+        const accessProfile = new AccessProfile();
         return await this.authService.validateVerificationCode(accessProfile, body);
     }
 
     @Post('refresh-token')
     refreshToken(@Body() { refreshToken }: RefreshTokenDto) {
         return this.authService.refreshToken(refreshToken);
+    }
+
+    @Post('change-password')
+    @UseGuards(AuthGuard('jwt'))
+    async changePassword(
+        @GetAccessProfile() accessProfile: AccessProfile,
+        @Body() changePasswordDto: ChangePasswordDto,
+    ) {
+        return this.authService.changePassword(accessProfile, changePasswordDto);
     }
 }
