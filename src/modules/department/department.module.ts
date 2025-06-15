8cc05865eaf5963bc275f1dbceeace127a756e2e
Rayandson <rayandson_silva@hotmail.com>
2025-06-14 21:57:54 -0300

    feat: throw error when trying to delete a department with users assigned

--- src/modules/department/department.module.ts
+++ src/modules/department/department.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
 import { DepartmentController } from './department.controller';
 import { DepartmentRepository } from './department.repository';
 import { DepartmentService } from './department.service';
 import { Department } from './entities/department.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Department])],
+    imports: [TypeOrmModule.forFeature([Department, User])],
     exports: [DepartmentService],
     controllers: [DepartmentController],
     providers: [DepartmentService, DepartmentRepository],
